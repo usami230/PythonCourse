e820f0a57f3ce626ed7876d82644b5c9f7f4d9c5
usami230 <53690847+usami230@users.noreply.github.com>
2024-05-22 11:36:11 -0700

    Class 1 intro

---
+++ Class1/main.py
@@ -0,0 +1,23 @@
+#   --------------Topic Comments
+# Method 1 Multi lines Comment
+
+"""
+print("")
+print("")
+print("")
+print("")
+
+"""
+
+# Method 2 Single lines Comment
+
+# open the website "https://www.online-python.com/"
+# multi-line comment
+# multi-line comment
+# multi-line comment
+# multi-line comment
+
+print("hello world")  # with double quotation marks
+print('hello world')  # with single quotation marks
+
+
