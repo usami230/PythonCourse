13aa116b416ae3bf34826f9bd79746bbe0114afa
usami230 <53690847+usami230@users.noreply.github.com>
2024-05-24 13:08:00 -0700

    Assignment

---
+++ Class2/assignment.py
@@ -0,0 +1,113 @@
+# problem 1
+
+# -------Start problem 1----------
+"""
+number = float(input("Enter a float number : "))
+print("round no : " + str(round(number,2)))
+print("orignal no : " + str(number))
+
+name = input("Enter a name : ")
+print("name len is : " +  str(len(name)))
+
+"""
+# -------End problem 1----------
+
+# problem 2
+
+# -------Start problem 2----------
+
+"""
+sentence = input("Enter a Sentence : ")
+
+print("Uppercase : " +  sentence.upper())
+print("lowercase : " +  sentence.lower())
+print("Capitalize : " +  sentence.capitalize())
+"""
+# -------End problem 2----------
+
+
+# problem 3
+
+# -------Start problem 3----------
+"""
+sentence = input("Enter a sentences : ")
+replaceworld = input("Enter Replace word : ")
+replaceworldwith = input("Enter Replace word with : ")
+
+print(sentence.replace(replaceworld,replaceworldwith))
+"""
+# -------End problem 3----------
+
+# problem 4
+# -------Start problem 4----------
+
+"""
+age = int(input("Enter age : "))
+add = 10
+print("In 10 years, you will be "+ str((age + add)) +" years old. where "+str((age + add)) +" is the new age.")
+"""
+# -------End problem 4----------
+
+# problem 5
+"""
+Write a program that:
+
+Asks the user to enter their full name.
+Converts the full name to uppercase and prints it.
+Asks the user for their favorite number.
+Multiplies the number by 2, converts it to a string, and concatenates it to a message.
+
+Prints the message "Your favorite number multiplied by 2 is X.", where X is the new number.
+"""
+
+# problem 4
+# -------Start problem 5----------
+"""
+full_name = input("Enter your full name: ")
+upper_name = full_name.upper()
+print("uppercase is:", upper_name)
+favorite_number = int(input("Enter your favorite number: "))
+new_number = favorite_number * 2
+message = "Your favorite number multiplied by 2 is " + str(new_number) + ".where " + str(new_number) + " is the new number."
+print(message)
+"""
+# -------End problem 5----------
+
+
+# problem 6
+
+# -------Start problem 6----------
+"""
+full_name = input("Enter first name and last name : ")
+print("upper case : "+full_name.upper() )
+print("Modified sentence: " + full_name.replace(" ","-"))
+print("The length of your full name is: " + str(len(full_name)))
+"""
+# -------End problem 6----------
+
+# problem 7
+
+# -------Start problem 7----------
+"""
+first = float(input("Enter First Number"))
+second = float(input("Enter Second Number"))
+avg = (first + second) / 2
+
+print(f"The avg number is " + str(round(avg,2)))
+"""
+# -------End problem 7----------
+
+# problem 8
+
+# -------Start problem 8----------
+"""
+
+sentence = input("Enter a sentence")
+print(sentence.replace(" ","_"))
+print(sentence.split(" "))
+
+"""
+# -------End problem 8----------
+
+
+
