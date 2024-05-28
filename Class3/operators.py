ea281bdef72d1ad95d51fe20dac91447a2c0784a
usami230 <53690847+usami230@users.noreply.github.com>
2024-05-28 12:02:02 -0700

    Operators

---
+++ Class3/operators.py
@@ -0,0 +1,110 @@
+#Arithmetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+
+
+#Arithmetic Operators
+# +	Addition
+# -	Subtraction
+# *	Multiplication
+# /	Division
+# %	Modulus
+# **	Exponentiation
+# //	Floor division
+
+#Assignment Operators
+# =	x = 5
+# +=	x += 3
+# -=	x -= 3
+
+#Comparison Operators
+#==	Equal
+#!=	Not equal
+#>	Greater than
+#<	Less than
+#>=	Greater than or equal to
+#>=	Greater than or equal to
+
+#Logical Operators
+#and 	Returns True if both statements are true
+#or	Returns True if one of the statements is true
+#not	Reverse the result, returns False if the result is true
+
+
+#x = 5 # value 5 is saved in the variale x
+#print(x) # 5
+
+## how to increment the value of x by 1
+#x = x + 1 #
+#print(x) # 6
+
+## another way of incrementing the value of variable
+#x += 1
+#print(x) # 7
+
+
+
+# other operators works similarly
+# subtraction
+#y = 10
+#print(y)  # 10
+#y -= 2  # 10 - 2
+#print(y)  # 8
+#y -= 5  # 8-5
+#print(y)  # 3
+
+
+# multiplication
+#y = 2
+#print(y) # 2
+#y *= 5 # 2 * 5
+#print(y) # 10
+#y *= 2 # 10 * 2
+#print(y) # 20
+
+# # division
+# y = 10
+# print(y)  # 10
+# y /= 2  # 10/2
+# print(y)  # 5.0
+# y /= 5  # 5.0/5
+# print(y)  # 1.0
+
+
+# floor division
+#y = 10
+#print(y)  # 10
+#y /= 2  # 10/2
+#print(y)  # 5
+#y /= 5  # 5/5
+#print(y)  # 1
+
+
+# NOTE: Python does not support post and pre increment operators
+# For example:
+# x++
+# ++x
+
+# comparison Operator
+
+#x = 5
+#y = 8
+
+#print("x == y:", x == y)  # False
+#print("x != y:", x != y)  # True
+#print("x < y:", x < y)    # True
+#print("x > y:", x > y)    # False
+#print("x <= y:", x <= y)  # True
+#print("x >= y:", x >= y)  # False
+
+
+# live coding
+#x = 100
+#y = 50
+#print(x == y)
+
+
+#x = "100"
+#y = 100
+#print(x == y)
