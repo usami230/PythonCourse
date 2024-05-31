7510df9240674eff8416b8ad9afc300e7bcec987
usami230 <53690847+usami230@users.noreply.github.com>
2024-05-31 10:35:11 -0700

    Assignment

---
+++ Class3/assignment.py
@@ -0,0 +1,48 @@
+# Q1 Write a program to check whether a person is eligible to vote or not?
+
+
+"""
+age = int(input("Enter Age :"))
+if age >= 18 :
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+"""
+
+# Q2 Write a program to check char is vowel or not.
+
+"""
+char = input("Enter Alphabet : ")
+if(char.lower() == "a" or 
+   char.lower() == "e" or 
+   char.lower() == "i" or 
+   char.lower() == "o" or 
+   char.lower() == "u"):
+
+    print("The character is a vowel")
+else :
+    print("The character is not a vowel")
+"""
+
+# Q3 Write a program to check the number is positive or negative. User input.
+
+"""
+number = int(input("Enter Number : "))
+if number < 0:
+    print("Negtive")
+else :
+   print("Positive")  
+"""
+
+# Q4 Write a program to check whether a number is odd or even?
+
+"""
+number = int(input("Enter Number : "))
+if number % 2 == 0 :
+    print("Even")
+else :
+    print("Odd")
+
+"""
+
+ # Q5 Write a program to display the grade of the user in subject A, ask user marks obtained out of 100
